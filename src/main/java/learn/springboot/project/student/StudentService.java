3e0cc0de62e68305e7cbe66911df921c0581b450
Jim Nguyen <vjimnguyen@gmail.com>
2021-06-28 13:35:04 -0700

    Added logic for updating student

--- src/main/java/learn/springboot/project/student/StudentService.java
+++ src/main/java/learn/springboot/project/student/StudentService.java
@@ -3,7 +3,9 @@ package learn.springboot.project.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -23,7 +25,7 @@ public class StudentService {
     public void addNewStudent(Student student) {
         Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
         if (studentOptional.isPresent()) {
-            throw new IllegalStateException("Email exists");
+            throw new IllegalStateException("email already exists");
         }
         studentRepository.save(student);
     }
@@ -31,9 +33,24 @@ public class StudentService {
     public void deleteStudent(Long studentId) {
         boolean exists = studentRepository.existsById(studentId);
         if (!exists) {
-            throw new IllegalStateException("Student with id " + studentId + " does not exist");
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("student with " + studentId + " does not exist"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
         }
 
-        studentRepository.deleteById(studentId);
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email already exists");
+            }
+            student.setEmail(email);
+        }
     }
 }
